ad683bf84b14ca2b1b9df955a4dbf0a7e7d50e9b
Islomjon <islomjonrahimjonov6474@gmail.com>
2024-05-12 23:52:31 +0500

    feat: initial commit.

---
+++ components/Button.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { observer } from 'mobx-react';
+
+const Button = observer(() => {
+    return (
+        <div>
+            
+        </div>
+    );
+});
+
+export default Button;
